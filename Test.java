eb5bc754982401352fc68762ff288404ee2ae27f
yangzhaoguang <zhaoguang.yang@fit2cloud.cn>
2024-07-04 22:22:24 +0800

    第一次提交

---
+++ Test.java
@@ -0,0 +1,11 @@
+/**
+ * @program: test-git-operation
+ * @description:
+ * @author: YZG
+ * @date: 2024-07-04 22:21
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("初始状态！～");
+    }
+}
